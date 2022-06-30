04853d71744d8d54e71fe5f6ca3d53ad2f4cee9d
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2022-06-30 02:52:07 -0300

    :bug: fix connection string

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -3,12 +3,21 @@ import mongoose from 'mongoose';
 export const connectMongoose = async () => {
   const url = process.env.DB_MONGO_URL;
   const name = process.env.DB_MONGO_NAME;
-  const port = process.env.DB_MONGO_PORT;
+  let port = process.env.DB_MONGO_PORT;
   const username = process.env.DB_MONGO_USERNAME;
   const password = process.env.DB_MONGO_PASSWORD;
+  const args = process.env.DB_MONGO_ARGS;
+
+  const uri = process.env.DB_MONGO_URI;
+
+  if (uri === 'mongodb+srv') {
+    port = '';
+  } else {
+    port = `:${process.env.DB_MONGO_PORT}`;
+  }
 
   await mongoose.connect(
-    `mongodb://${username}:${password}@${url}:${port}/${name}?authSource=admin&readPreference=primary&directConnection=true&ssl=false`
+    `${uri}://${username}:${password}@${url}${port}/${name}${args}`
   );
 
   console.log(`\n        💾 Conexao do mongo estabilizada\n`);
